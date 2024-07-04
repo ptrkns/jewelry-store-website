57f5a1b26e7d36cc237388bc62821918ff2b519a
ptrkns <keneseipeterr@gmail.com>
2024-07-04 14:09:22 +0200

    Customized the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,17 +4,17 @@ import logo from '../assets/logo-1.png';
 
 function Navbar() {
 
-  return (
-    <div className="flex flex-row justify-between items-center px-10 py-5">
-        <img className="w-1/4" src={logo}/>
+    return (
+    <div className="flex flex-col md:flex-row justify-between items-center px-10 py-5">
+        <img className="md:w-1/4 pb-2" src={logo}/>
         <div>
-            <ul className="flex flex-row justify-evenly items-center space-x-5">
+            <ul className="flex flex-row justify-evenly items-center space-x-5 pb-2">
                 <li><Link className="py-5 border-b-4 border-white hover:border-gray-500 transition duration-300" to='/'>Home</Link></li>
                 <li><Link className="py-5 border-b-4 border-white hover:border-gray-500 transition duration-300" to='/about'>About</Link></li>
             </ul>
         </div>
     </div>
-  );
+    );
 }
 
 export default Navbar;
